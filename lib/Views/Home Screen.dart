1ca9e9599bb077cbcb9b0400397efcb0ba8409bb
Ma7moudSelmy <Ma7moud.m.selmy1@gmail.com>
2025-06-13 19:42:05 +0300

    Enhance Homescreen layout with AppBar and action buttons

--- lib/Views/Home Screen.dart
+++ lib/Views/Home Screen.dart
@@ -3,14 +3,36 @@ import 'package:flutter/material.dart';
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
 
-  
-  
   State<Homescreen> createState() => _MyWidgetState();
 }
 
 class _MyWidgetState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 199, 200, 202),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 4, 210, 247),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.storefront),
+            onPressed: () {
+              // Action for shopping cart
+            },
+          ),
+          Text(
+            "61 Hopper street..",
+            style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(width: 230),
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {
+              // Action for shopping cart
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-import 'package:ecommerce_fruit_shop/Views/Home%20Screen.dart';
 import 'package:ecommerce_fruit_shop/Views/Splash%20Screen.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-
+void main() {
   runApp(const MyWidget());
 }
+
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
@@ -13,6 +12,7 @@ class MyWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Splashscreen(),);
+      home: Splashscreen(),
+    );
   }
 }
